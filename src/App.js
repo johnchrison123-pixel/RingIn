444e8fa65de0ac2062ee3e1cf1f95c584eab5340
Your Namejoe <johnchrison123@gmail.com>
2026-05-11 23:17:46 +0400

    Fix blank screen: missing useRef import in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import HomeScreen, {UserProfileView} from './screens/HomeScreen';
 import {useFollow} from './screens/useFollow';
